bf551192dae8c445633cde3a436ce85ced1949ea
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-10-26 23:54:39 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
 
-end
+    # Getter methods
+    def name
+     @name
+    end
+    
+    def breed
+     @breed
+    end
+   
+    # Setter methods
+    def name=(new_name)
+     @name = new_name
+    end
+    
+    def breed=(dog_breed)
+     @breed = dog_breed
+    end
+   
+   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,21 @@
 class Person
 
-end
+    # Getter methods
+    def name
+     @name
+    end
+   
+    def job
+     @job
+    end
+   
+    # Setter methods
+    def name=(new_name)
+     @name = new_name
+    end
+   
+    def job=(new_job)
+     @job = new_job
+    end
+   
+   end
